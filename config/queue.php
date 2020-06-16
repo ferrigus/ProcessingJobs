968ab6e16753e713ed41cfcf32278109af0b67d9
Felix Utrera <ferrigus@hotmail.com>
2020-06-16 00:57:44 -0400

    Additional config

--- config/queue.php
+++ config/queue.php
@@ -37,7 +37,7 @@ return [
         'database' => [
             'driver' => 'database',
             'table' => 'jobs',
-            'queue' => 'default',
+            'queue' => 'high',
             'retry_after' => 90,
         ],
 
